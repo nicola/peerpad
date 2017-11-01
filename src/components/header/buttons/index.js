01e7b27585e53efd8ece977fb5204000e7628a41
nicola <me@nicola.io>
2017-11-01 12:29:23 +0000

    making gh-pages

--- src/components/header/buttons/index.js
+++
@@ -1,3 +0,0 @@
-export { default as NewButton } from './NewButton'
-export { default as NotificationsButton } from './NotificationsButton'
-export { default as PeersButton } from './PeersButton'
--- src/components/toolbar/buttons/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './Button'
-export { default as LinkButton } from './LinkButton'
-export { default as SnapshotsButton } from './SnapshotsButton'
